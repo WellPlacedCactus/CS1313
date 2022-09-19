ac4b1bc1d5faa686d5d0a6c3dac5a7542885672b
WellPlacedCactus <abowlasoup@gmail.com>
2022-09-19 13:55:42 -0500

    First ever

---
+++ PP2/census.c
@@ -0,0 +1,133 @@
+
+/*
+ * Project #2:  Forced US Census Bureau Survey
+ * Program:     census
+ * Author:      Nathan Vu (nathan.v.vu-1@ou.edu)
+ * Class:       CS1313 010 Programming for Non-Majors in C, Fall 2022
+ * Lab:         Sec 010 Fridays 10:30am
+ * Description: Forces the user to take a US Census Bureau Survey
+ *              for data collection purposes. Asks the user to
+ *              input information reguarding their social media
+ *              usage and phone number credentials.
+*/
+
+#include <stdio.h>
+
+int main()
+{ /* main */
+
+ /*
+  * DECLARATION SECTION
+  */
+
+ /*
+  * NATURE AND PURPOSE OF EACH VARIABLE NAME
+  * 
+  * average_text_messages_received_per_day:     
+  *                                         The subjects's average
+  *                                         number of text messages
+  *                                         received per day should
+  *                                         be this.
+  * average_web_videos_watched_per_week:
+  *                                         The subjects's average
+  *                                         number of web videos
+  *                                         watched per week should
+  *                                         be this.
+  * first_three_digits_of_phone_number:
+  *                                         The subject's first
+  *                                         three phone number digits
+  *                                         should be this. This is
+  *                                         also commonly referred to
+  *                                         as the area code.
+  * second_three_digits_of_phone_number:
+  *                                         The subject's second three
+  *                                         phone number digits should
+  *                                         be this.
+  * last_four_digits_of_phone_number:
+  *                                         The subject's final four
+  *                                         phone number digits should
+  *                                         be this.
+  */
+
+  float average_text_messages_received_per_day;
+  float average_web_videos_watched_per_week;
+  int   first_three_digits_of_phone_number;
+  int   second_three_digits_of_phone_number;
+  int   last_four_digits_of_phone_number;
+
+ /*
+  * EXECUTION SECTION
+  */
+
+ /*
+  * GREETING SUBSECTION
+  */
+
+ /*
+  * Inform user of survey they must take
+  */
+  printf("You are now being forced to participate\n");
+  printf("in a survey conducted by the US Census\n");
+  printf("Bureau. You will now be asked a series\n");
+  printf("of questions against your will and you\n");
+  printf("WILL answer honestly.\n");
+
+ /*
+  * INPUT SUBSECTION
+  */
+
+ /*
+  * Prompt user of how many text messages they
+  * receive in one day
+  */
+  printf("On average, how many text messages do\n");
+  printf("you receive in one day?\n");
+
+ /*
+  * Input how many text messages user receives in one day
+  */
+  scanf("%f", &average_text_messages_received_per_day);
+
+ /*
+  * Prompt user of how many web videos they watch in one week
+  */
+  printf("On average, how many web videos do you\n");
+  printf("watch in one week?\n");
+
+ /*
+  * Input how many web videos user watches in one week
+  */
+  scanf("%f", &average_web_videos_watched_per_week);
+
+ /*
+  * Prompt user of their phone number
+  */
+  printf("What is your phone number?\n");
+
+ /*
+  * User inputs their phone number separated by spaces
+  */
+  scanf("%d %d %d",
+    &first_three_digits_of_phone_number,
+    &second_three_digits_of_phone_number,
+    &last_four_digits_of_phone_number);
+
+ /*
+  * OUTPUT SUBSECTION
+  */
+
+ /*
+  * Display all of the information just collected
+  */
+  printf("On average,\n");
+  printf("you receive %f text messages in one\n",
+    average_text_messages_received_per_day);
+  printf("day and you watch %f web videos in\n",
+    average_web_videos_watched_per_week);
+  printf("one week.\n");
+  printf("Your phone number is %d-%d-%d\n",
+    first_three_digits_of_phone_number,
+    second_three_digits_of_phone_number,
+    last_four_digits_of_phone_number);
+
+} /* main */
---
+++ PP2/read_list.c
@@ -0,0 +1,108 @@
+
+/**
+ * Project #2:		Example Program for PP#2
+ * Program:				read_list
+ * Author:				Nathan Vu (nathan.v.vu-1@ou.edu)
+ * Class:					CS1313 010 Programming for Non-Majors in C, Fall 2022
+ * Lab:						Sec 010 Fridays 10:30am
+ * Description:		Copy of Dr. Neemans example program hand copied line
+ * 								by line from "Standard I/O Lesson" on slide 20
+ * 								@ cs1313.ou.edu
+*/
+
+#include <stdio.h>
+
+int main()
+{ /* main */
+
+ /*
+  * DECLARATION SECTION
+  */
+
+ /*
+  * NATURE AND PURPOSE OF EACH VARIABLE NAME
+  * 
+  * CS1313_average_height_in_m:     
+  *                               The average height of all
+  *                               students in CS1313 measured
+  *                               in units of meters should
+  *                               be this.
+  * number_of_silly_people:
+  *                               The number of all silly
+  *                               people measured in humans
+  *                               should be this.
+  * number_of_nonsilly_people:
+  *                               The number of all nonsilly
+  *                               people measured in humans
+  *                               should be this.
+  * Henrys_middle_initial:
+  *                               Dr. Neeman's middle initial
+  *                               should be this.
+  */
+
+	float	CS1313_average_height_in_m;
+	int		number_of_silly_people;
+	int		number_of_nonsilly_people;
+	char	Henrys_middle_initial;
+
+ /*
+  * EXECUTION SECTION
+  */
+
+ /*
+  * GREETING SUBSECTION
+  */
+
+ /*
+  * Inform the user that they will be asked some questions
+  */
+	printf("I'm going to guess the answers to questions\n");
+	printf("I've already asked!\n");
+
+ /*
+  * INPUT SUBSECTION
+  */
+
+ /*
+  * Prompt the user of how many silly and nonsilly people there are
+  */
+	printf("In CS1313, how many silly people are there,\n");
+	printf(",and how many non-silly people are there?\n");
+
+ /*
+  * Input how many silly and nonsilly people there are
+  */
+	scanf("%d %d",
+		&number_of_silly_people,
+		&number_of_nonsilly_people);
+
+ /*
+  * Promps the user of the average height of students in
+  * CS1313 as well as Henry's middle initial
+  */
+	printf("What is the average height in m in CS1313\n");
+	printf(",and what is Henry's middle initial?\n");
+
+ /*
+  * Inputs the average height and middle initial
+  */
+	scanf("%f %c",
+		&CS1313_average_height_in_m,
+		&Henrys_middle_initial);
+
+ /*
+  * OUTPUT SUBSECTION
+  */
+
+ /*
+  * Outputs all of the information gathered in the previous moments
+  */
+	printf("In CS1313, there are %d silly people\n",
+		number_of_silly_people);
+	printf("and %d non-silly people.\n",
+		number_of_nonsilly_people);
+	printf("In CS1313, the average height is %f m.\n",
+		CS1313_average_height_in_m);
+	printf("Henry's middle initial is %c.\n",
+		Henrys_middle_initial);
+} /* main */
